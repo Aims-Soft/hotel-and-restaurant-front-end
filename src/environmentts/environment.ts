02c8ebc4201d90206713af29ef72e1d177a70514
MuhammadLuqman123 <mluqman9154651@gmail.com>
2026-03-06 12:30:39 +0500

    update designing

--- src/environmentts/environment.ts
+++ src/environmentts/environment.ts
@@ -5,11 +5,11 @@
 export const environment = {
   production: false,
   envName: 'prod',
-  // apiUrlauth: 'https://hotelandrestaurantstaffportalofpakistan.com:9081/',
-  apiUrlauth: 'http://localhost:9081/',
+  apiUrlauth: 'https://hotelandrestaurantstaffportalofpakistan.com:9081/',
+  // apiUrlauth: 'http://localhost:9081/',
 
-  apiUrl: 'http://localhost:9081/',
-  // apiUrl: 'https://hotelandrestaurantstaffportalofpakistan.com:9081/',
+  // apiUrl: 'http://localhost:9081/',
+  apiUrl: 'https://hotelandrestaurantstaffportalofpakistan.com:9081/',
   // apiUrl: 'http://159.69.174.28:6601/',
   imageUrl: 'C:/inetpub/wwwroot/jobPortal/jobPortal-app/assets/Job-images/',
 
